1671851d5bfeb1ca510649010fcd1f81c37098a9
Ogala Theodore <theobanskid45@gmail.com>
2024-10-25 11:31:07 +0100

    second commit

---
+++ version.php
@@ -0,0 +1,27 @@
+<?php
+    $project_name = 'AmazCart';
+    function getStorageFile($file_name){
+        $file = 'storage/app/.'.$file_name;
+        if(file_exists($file)){
+            return file_get_contents($file);
+        }
+        return false;
+    }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="author" content="spondonit">
+    <meta name="descripton" content="A project of spondonit">
+    <title>Version | <?php echo $project_name ?></title>
+</head>
+
+<body>
+    <h1>Your current version of <?php  echo $project_name . ' is '. getStorageFile('version') ?></h1>
+</body>
+
+</html>
+
+
+
